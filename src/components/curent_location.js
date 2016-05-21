aa6293c23b6beb219f7bd469cb9186cb44975125
Radislaw <radislaw22@gmail.com>
2016-05-21 15:44:47 +1000

    modify location styles

--- src/components/curent_location.js
+++ src/components/curent_location.js
@@ -4,8 +4,8 @@ import Clock from './time';
 class CurentLocation extends Component {
   render() {
     return (
-      <div className="col-md-4 col-md-offset-4">
-        <i className="wi-location"></i>
+      <div className="col-md-4 col-md-offset-4 text-right">
+        <i className="wi-location location_icon"></i>
         <p className="card__meta">Vladivostok, Russia</p>
         <Clock />
       </div>
